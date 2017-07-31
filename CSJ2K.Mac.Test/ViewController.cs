e0c020f774710613e2aefcef6a222000b075686e
Satoshi ARA <satoshi.ara@newton-graphics.co.jp>
2017-07-31 17:01:44 +0900

    added support for xamarin.mac. Note: System.Drawing namespace can not be used in xamarin.mac

---
+++ CSJ2K.Mac.Test/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+using CoreGraphics;
+
+namespace CSJ2K.Mac.Test
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+			// Do any additional setup after loading the view.
+			using (var stream = typeof(ViewController).Assembly.GetManifestResourceStream("CSJ2K.Mac.Test.Files.file1.jp2"))
+			{
+				var uiImage = J2kImage.FromStream(stream).As<NSImage>();
+				var imageView = new NSImageView(new CGRect(0, 0, uiImage.Size.Width, uiImage.Size.Height)) { Image = uiImage };
+				this.View.AddSubview(imageView);
+			}
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
---
+++ CSJ2K/Util/MacImage.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2007-2016 CSJ2K contributors.
+// Licensed under the BSD 3-Clause License.
+
+namespace CSJ2K.Util
+{
+    using CoreGraphics;
+
+    using CoreImage;
+
+    using AppKit;
+
+    internal class MacImage : ImageBase<CGImage>
+    {
+        #region CONSTRUCTORS
+
+        internal MacImage(int width, int height, byte[] bytes)
+            : base(width, height, bytes)
+        {
+        }
+
+        #endregion
+
+        #region METHODS
+
+        public override T As<T>()
+        {
+            if (typeof(T) == typeof(NSImage))
+            {
+                var cgImage = (CGImage)this.GetImageObject();
+                return (T)(object)new NSImage(cgImage, new CGSize(cgImage.Width, cgImage.Height));
+            }
+
+            if (typeof(T) == typeof(CIImage))
+            {
+                return (T)(object)new CIImage((CGImage)this.GetImageObject());
+            }
+
+            return (T)this.GetImageObject();
+        }
+
+        protected override object GetImageObject()
+        {
+            var length = this.Bytes.Length;
+            var bytes = new byte[length];
+
+            // Switch byte representation from BGRA to RGBA
+            for (var k = 0; k < length; k += 4)
+            {
+                bytes[k] = this.Bytes[k + 2];
+                bytes[k + 1] = this.Bytes[k + 1];
+                bytes[k + 2] = this.Bytes[k];
+                bytes[k + 3] = this.Bytes[k + 3];
+            }
+
+            using (
+                var context = new CGBitmapContext(
+                    bytes,
+                    this.Width,
+                    this.Height,
+                    8,
+                    SizeOfArgb * this.Width,
+                    CGColorSpace.CreateDeviceRGB(),
+                    CGImageAlphaInfo.PremultipliedLast))
+            {
+                return context.ToImage();
+            }
+        }
+
+        #endregion
+    }
+}
